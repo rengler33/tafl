12a483d223b13b5d52746fba3ebce60bace778ea
Robert Engler <rengler33@gmail.com>
2022-09-29 11:21:12 -0400

    Remove old capture code

--- tafl/lib/impl/captures.ex
+++ tafl/lib/impl/captures.ex
@@ -1,7 +1,7 @@
 defmodule Tafl.Impl.Captures do
-  alias Tafl.Impl.{Piece, Space, Spaces}
+  alias Tafl.Impl.{Space, Spaces}
 
-  def perform_captures3(spaces, player, new_location) do
+  def perform_captures(spaces, player, new_location) do
     coord_map = Spaces.coord_map(spaces, new_location)
     # left, right, up, down
     [{0, -1}, {0, 1}, {-1, 0}, {1, 0}]
@@ -87,6 +87,8 @@ defmodule Tafl.Impl.Captures do
   defp space_can_capture?(_, _), do: false
 
   defp capture({{r_rel, c_rel}, captured?}, spaces, {r, c}) when captured? == true do
+    # TODO decide - if capturing king, i don't actually want to capture
+    # but do want to return game state over? maybe not, maybe this is better
     location = {r_rel + r, c_rel + c}
     IO.inspect(location, label: "CAPTURING")
 
@@ -97,145 +99,7 @@ defmodule Tafl.Impl.Captures do
     new_spaces
   end
 
-  defp capture(_, spaces, _) do
-    spaces
-  end
-
-  # def perform_captures2(spaces, player, new_location) do
-  #   x = Spaces.index_on_flat_board(new_location)
-  #   b = Spaces.board_size()
-
-  #   chunks_flat =
-  #     [
-  #       # up, left, right, down
-  #       [x - 2 * b, x - b, x],
-  #       [x - 2, x - 1, x],
-  #       [x, x + 1, x + 2],
-  #       [x, x + b, x + 2 * b]
-  #     ]
-  #     # drop any chunks that contain a negative number
-  #     |> Enum.map(fn values ->
-  #       if Enum.any?(values, fn val -> val < 0 end), do: [], else: values
-  #     end)
-  #     |> IO.inspect()
-  #     |> List.flatten()
-
-  #   chunk_set = MapSet.new(chunks_flat)
-  #   IO.inspect(chunk_set)
-
-  #   pieces =
-  #     spaces
-  #     |> List.flatten()
-  #     |> Enum.with_index()
-  #     |> Enum.filter(fn {_, ind} -> ind in chunk_set end)
-  #     |> Enum.map(fn {val, ind} -> {ind, val} end)
-  #     |> Map.new()
-
-  #   IO.inspect(pieces)
-
-  #   chunks =
-  #     chunks_flat
-  #     |> Enum.map(fn val -> {val, pieces[val]} end)
-  #     |> Enum.chunk_every(3)
-
-  #   IO.inspect(chunks)
-  #   spaces
-
-  # using index counts on a flattened spaces:
-  # get 2 upper spaces, 2 left spaces, 2 right spaces, & 2 lower spaces
-  # (dropping any negative index values)
-  # upper and lower calculations can use a multiple of board_size
-  # then check each chunk for a capture
-  # then replace each space at the capture index with null piece, if captured
-  # then chunk flattened list back into rows
-
-  # nevermind, this is incomplete - my formula sucks because a right edge
-  # piece plus two actually gives a piece from next row when dealing with
-  # flattened list structure
-
-  # might as well just be a little inefficient and use the collect_spaces
-  # functions i already have (just need to make the option to being inclusive
-  # of the original space, + 2 spaces on each side)
-  # end
-
-  def perform_captures(spaces, player) do
-    spaces_after_horizontal_capture =
-      spaces
-      |> Enum.map(&perform_row_captures(&1, player))
-
-    spaces_after_vertical_capture_too =
-      spaces_after_horizontal_capture
-      |> Spaces.transpose()
-      |> Enum.map(&perform_row_captures(&1, player))
-      |> Spaces.transpose()
-
-    spaces_after_vertical_capture_too
-  end
-
-  def perform_row_captures(row, player) do
-    middle_values =
-      row
-      |> Enum.chunk_every(3, 1, :discard)
-      |> Enum.map(&do_capture_on_chunk(&1, player))
-      # unchunk by taking the middle (updated) spaces
-      |> Enum.map(&Enum.at(&1, 1))
-      |> List.flatten()
-
-    # add back the first and last space
-    [Enum.at(row, 0), middle_values, List.last(row)] |> List.flatten()
-  end
-
-  defp do_capture_on_chunk(chunk, player) do
-    other_player = other_player(player)
-
-    case chunk do
-      [
-        s1 = %{kind: :corner},
-        s2 = %{piece: %{owner: ^other_player}},
-        s3 = %{piece: %{owner: ^player}}
-      ] ->
-        [s1, capture_piece(s2), s3]
-
-      [
-        s1 = %{piece: %{owner: ^player}},
-        s2 = %{piece: %{owner: ^other_player}},
-        s3 = %{piece: %{owner: ^player}}
-      ] ->
-        [s1, capture_piece(s2), s3]
-
-      [
-        s1 = %{piece: %{owner: ^player}},
-        s2 = %{piece: %{owner: ^other_player}},
-        s3 = %{kind: :corner}
-      ] ->
-        [s1, capture_piece(s2), s3]
-
-      [
-        s1 = %{piece: %{owner: ^player}},
-        s2 = %{piece: %{owner: ^other_player}},
-        s3 = %{kind: :center}
-      ] ->
-        [s1, capture_piece(s2), s3]
-
-      [
-        s1 = %{kind: :center},
-        s2 = %{piece: %{owner: ^other_player}},
-        s3 = %{piece: %{owner: ^player}}
-      ] ->
-        [s1, capture_piece(s2), s3]
-
-      _ ->
-        chunk
-    end
-  end
-
-  defp capture_piece(space) do
-    case space.piece.kind do
-      # king is uncapturable (signals end of game win condition)
-      :king -> space
-      _ -> %Space{space | piece: %Piece{}}
-    end
-  end
+  defp capture(_, spaces, _), do: spaces
 
   # TODO refactor, copying from Game
   defp other_player(:p1), do: :p2
--- tafl/lib/impl/game.ex
+++ tafl/lib/impl/game.ex
@@ -77,9 +77,7 @@ defmodule Tafl.Impl.Game do
   #########################################
 
   defp perform_captures(game, new_location) do
-    new_spaces = Captures.perform_captures3(game.spaces, game.turn, new_location)
-    # new_spaces = Captures.perform_captures2(game.spaces, game.turn, new_location)
-    # new_spaces = Captures.perform_captures(game.spaces, game.turn)
+    new_spaces = Captures.perform_captures(game.spaces, game.turn, new_location)
     %__MODULE__{game | spaces: new_spaces}
   end
 
